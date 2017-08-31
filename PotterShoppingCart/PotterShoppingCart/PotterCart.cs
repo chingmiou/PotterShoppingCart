9e714fcc8588c5aff6ca6daae7b806fefb6a8b19
JANET\JanetWang <ching.miou@gmail.com>
2017-08-31 21:49:32 +0800

    Red1:第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ PotterShoppingCart/PotterShoppingCart/PotterCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PotterShoppingCart
+{
+    public class PotterCart
+    {
+        public PotterCart()
+        {
+        }
+
+        public int GetCheckoutPrice(List<PotterSeries> potterList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotterShoppingCart/PotterShoppingCartTests/PotterCartTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+
+namespace PotterShoppingCartTests
+{
+    [TestClass]
+    public class PotterCartTests
+    {
+        [TestMethod]
+        public void Test_Only_Buy_One_Episode1_CheckoutPrice_Should_Be_100()
+        {
+            var target = new PotterCart();
+            var potterList = new List<PotterSeries>
+            {
+                new PotterSeries { Episode = 1, Quantity = 1, Price = 100 },
+                new PotterSeries { Episode = 2, Quantity = 0, Price = 100 },
+                new PotterSeries { Episode = 3, Quantity = 0, Price = 100 },
+                new PotterSeries { Episode = 4, Quantity = 0, Price = 100 },
+                new PotterSeries { Episode = 5, Quantity = 0, Price = 100 }
+            };
+            var expected = 100;
+
+            var actual = target.GetCheckoutPrice(potterList);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
