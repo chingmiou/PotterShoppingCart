7d6cc29f935ba83a96c827ec2a9081ee87ffa58b
JANET\JanetWang <ching.miou@gmail.com>
2017-08-31 23:04:47 +0800

    Red_6:一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370

--- PotterShoppingCart/PotterShoppingCartTests/PotterCartTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/PotterCartTests.cs
@@ -101,5 +101,24 @@ namespace PotterShoppingCartTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_Buy_One_Episode1_And_One_Episode2_And_Two_Episode3_CheckoutPrice_Should_Be_370()
+        {
+            var target = new PotterCart();
+            var cartList = new List<PotterSeries>
+            {
+                new PotterSeries { Episode = 1, Quantity = 1, Price = 100 },
+                new PotterSeries { Episode = 2, Quantity = 1, Price = 100 },
+                new PotterSeries { Episode = 3, Quantity = 2, Price = 100 },
+                new PotterSeries { Episode = 4, Quantity = 0, Price = 100 },
+                new PotterSeries { Episode = 5, Quantity = 0, Price = 100 }
+            };
+            var expected = 370;
+
+            var actual = target.GetCheckoutPrice(cartList);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
