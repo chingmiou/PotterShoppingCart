9f644b77c73596901bc19ac72867d377ec71a65b
JANET\JanetWang <ching.miou@gmail.com>
2017-08-31 22:10:19 +0800

    Red_2:第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190

--- PotterShoppingCart/PotterShoppingCartTests/PotterCartTests.cs
+++ PotterShoppingCart/PotterShoppingCartTests/PotterCartTests.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PotterShoppingCart;
+using System.Collections.Generic;
 
 namespace PotterShoppingCartTests
 {
@@ -23,6 +22,25 @@ namespace PotterShoppingCartTests
             var expected = 100;
 
             var actual = target.GetCheckoutPrice(potterList);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Buy_One_Episode1_And_One_Episode2_CheckoutPrice_Should_Be_190()
+        {
+            var target = new PotterCart();
+            var cartList = new List<PotterSeries>
+            {
+                new PotterSeries { Episode = 1, Quantity = 1, Price = 100 },
+                new PotterSeries { Episode = 2, Quantity = 1, Price = 100 },
+                new PotterSeries { Episode = 3, Quantity = 0, Price = 100 },
+                new PotterSeries { Episode = 4, Quantity = 0, Price = 100 },
+                new PotterSeries { Episode = 5, Quantity = 0, Price = 100 }
+            };
+            var expected = 190;
+
+            var actual = target.GetCheckoutPrice(cartList);
 
             Assert.AreEqual(expected, actual);
         }
